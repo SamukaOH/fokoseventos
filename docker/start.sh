53926c768b45463c7ffbb73c7d6b9f4baa7095ce
SamukaOH <smena.mena@icloud.com>
2026-07-14 11:11:22 -0300

    fix: force LF + version marker v7.3.9

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# Railway: porta dinâmica
+# Railway: porta dinâmica + inicialização
+echo "=== FOKOS START v7.3.9 ==="
 sed -i "s/NGINX_PORT/${PORT:-8080}/g" /etc/nginx/http.d/default.conf
-
-# PHP-FPM em background
 php-fpm -D
-
-# Nginx em foreground
 exec nginx -g "daemon off;"
